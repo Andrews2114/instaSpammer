bc3a6be5a7452e711f07e725cd0326c54e8c1785
Andrews2114 <72770479+Andrews2114@users.noreply.github.com>
2021-07-23 14:51:50 -0500

    spam for owo

--- spammerXD.py
+++ spammerXD.py
@@ -1,6 +1,9 @@
+#C:\Users\4ndre\Documents\Python\Pruebas\Python_P\Codes\SpammerInsta\instaSpammer
+
 import pyautogui, time
 
 time.sleep(5)
 for word in range(100):
-    pyautogui.typewrite("Marxism does not work")
+    pyautogui.typewrite("owoh")
     pyautogui.press('enter')
+    time.sleep(15.5)
